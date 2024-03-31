cd5120bfbb9f64dfd93759ad1512f4c13989fdb6
Warrakyy <yahiaahmed128@gmail.com>
2024-03-31 23:10:49 +0200

    added images,removed redundant html

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
